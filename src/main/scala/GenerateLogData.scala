7cf19372a628b4fd2fc2833a80e8daa999ade5fe
pdeshp8 <pdeshp8@uic.edu>
2021-11-01 02:59:23 -0500

    - To try on EC2 (added assembly.sbt) try 2

--- src/main/scala/GenerateLogData.scala
+++ src/main/scala/GenerateLogData.scala
@@ -26,25 +26,26 @@ object GenerateLogData:
 
 //this is the main starting point for the log generator
 @main def runLogGenerator =
-  import Generation.RSGStateMachine.*
-  import Generation.*
-  import HelperUtils.Parameters.*
-  import GenerateLogData.*
-
-  logger.info("Log data generator started...")
-  val INITSTRING = "Starting the string generation"
-  val init = unit(INITSTRING)
-
-  val logFuture = Future {
-    LogMsgSimulator(init(RandomStringGenerator((Parameters.minStringLength, Parameters.maxStringLength), Parameters.randomSeed)), Parameters.maxCount)
-  }
-  Try(Await.result(logFuture, Parameters.runDurationInMinutes)) match {
-    case Success(value) => logger.info(s"Log data generation has completed after generating ${Parameters.maxCount} records.")
-    case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
-  }
+//  import Generation.RSGStateMachine.*
+//  import Generation.*
+//  import HelperUtils.Parameters.*
+//  import GenerateLogData.*
+//
+//  logger.info("Log data generator started...")
+//  val INITSTRING = "Starting the string generation"
+//  val init = unit(INITSTRING)
+//
+//  val logFuture = Future {
+//    LogMsgSimulator(init(RandomStringGenerator((Parameters.minStringLength, Parameters.maxStringLength), Parameters.randomSeed)), Parameters.maxCount)
+//  }
+//  Try(Await.result(logFuture, Parameters.runDurationInMinutes)) match {
+//    case Success(value) => logger.info(s"Log data generation has completed after generating ${Parameters.maxCount} records.")
+//    case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
+//  }
   val bucket_name: String = "logsinput"
-  val file_path: String = "./log/log.log"
+  val file_path: String = "log/log.log"
   val key_name: String ="log.log"
+  System.out.println(new File(file_path).isFile)
   val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_2).build
   try s3.putObject(bucket_name, key_name, new File(file_path))
   catch {
