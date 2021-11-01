d59dd83b044b198ea39eaf4aa295a785e4a62f7f
pdeshp8 <pdeshp8@uic.edu>
2021-11-01 02:26:21 -0500

    - Intermediate commit1

--- src/main/scala/GenerateLogData.scala
+++ src/main/scala/GenerateLogData.scala
@@ -7,6 +7,7 @@
  *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
  *
  */
+
 import Generation.{LogMsgSimulator, RandomStringGenerator}
 import HelperUtils.{CreateLogger, Parameters}
 import com.amazonaws.AmazonServiceException
