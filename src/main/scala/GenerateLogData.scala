93cd1b93057c390071874ebd1680b562fe8ab1f3
parth-d <pdeshp8@uic.edu>
2021-11-03 03:32:11 -0500

    -Optimizations

--- src/main/scala/GenerateLogData.scala
+++ src/main/scala/GenerateLogData.scala
@@ -44,14 +44,3 @@ object GenerateLogData:
       case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
     }
     putS3
-//    val bucket_name: String = "logsinput"
-//    val file_path: String = "log/log.log"
-//    val key_name: String = Paths.get(file_path).getFileName.toString
-//    System.out.println(new File(file_path).isFile)
-//    val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_2).build
-//    try s3.putObject(bucket_name, key_name, new File(file_path))
-//    catch {
-//      case e: AmazonServiceException =>
-//        System.err.println(e)
-//        System.exit(1)
-//    }
--- src/main/scala/putS3.scala
+++ src/main/scala/putS3.scala
@@ -1,14 +1,19 @@
 import com.amazonaws.AmazonServiceException
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.amazonaws.regions.Regions
+import com.typesafe.config.ConfigFactory
+
 import java.io.File
 
 object putS3:
-  val bucket_name: String = "logsinput"
-  val file_path: String = "./log/log.log"
-  val key_name: String ="log.log"
+  val config = ConfigFactory.load()
+  val bucket_name: String = config.getString("s3.bucket")
+  val file_path: String = config.getString("s3.file_path")
+  val key_name: String = config.getString("s3.key")
+  
   val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_2).build
   try s3.putObject(bucket_name, key_name, new File(file_path))
+    
   catch {
     case e: AmazonServiceException =>
       System.err.println(e)
